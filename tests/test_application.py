53148b878700a84db71a8a51d964751e85d1626e
Martin Nanchev <martin.nanchev@adesso.bg>
2022-01-05 18:42:08 +0200

    [restful_password_generator] - finish testing

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,4 +1,5 @@
 import json
+from io import BytesIO
 
 from flask_testing import TestCase
 
@@ -126,7 +127,7 @@ class TestApp(TestCase):
                     "ExcludeUppercase": True,
                     "ExcludeLowercase": True,
                     "IncludeSpace": False,
-                    "password": "pesho",
+                    "password": creator.password,
                 }
             ),
             headers=self.HEADERS,
@@ -137,7 +138,7 @@ class TestApp(TestCase):
         """
         Test the generate secret route
         """
-        self.HEADERS["Authorization"] = f"Bearer eaas"
+        self.HEADERS["Authorization"] = f"Bearer eYiQqQ"
         resp = self.client.post(
             self.GENERATE_SECRET_PATH,
             data=json.dumps(
@@ -149,31 +150,194 @@ class TestApp(TestCase):
                     "ExcludeUppercase": True,
                     "ExcludeLowercase": True,
                     "IncludeSpace": False,
-                    "password": "pesho",
+                    "password": self.HEADERS,
                 }
             ),
             headers=self.HEADERS,
         )
         self.assertEqual(401, resp.status_code)
 
-    def test_get_secret_with_predefined_secret(self):
+    def test_get_secret(self):
         """
         Test the get secret route
         """
-        # creator = CreatorFactory()
-        # reg_resp = self.client.post(
-        #     self.REGISTER_PATH,
-        #     data=json.dumps({"email": creator.email, "password": creator.password}),
-        #     headers=self.HEADERS,
-        # )
-        # secret = SecretFactory()
-        # self.client.post(
-        #     self.REGISTER_PATH,
-        #     data=json.dumps({"email": creator.email, "password": creator.password}),
-        #     headers=self.HEADERS,
-        # )
-        # resp = self.client.get(
-        #     f"/getSecret/{secret.id}", headers=self.HEADERS
-        # )
-        # self.assertEqual(200, resp.status_code)
-        # """
+        creator = CreatorFactory()
+
+        reg_resp = self.client.post(
+            self.REGISTER_PATH,
+            data=json.dumps({"email": creator.email, "password": creator.password}),
+            headers=self.HEADERS,
+        )
+        self.HEADERS["Authorization"] = f"Bearer {reg_resp.json['token']}"
+        gen_secret_resp = self.client.post(
+            self.GENERATE_SECRET_PATH,
+            data=json.dumps(
+                {
+                    "PasswordLength": 255,
+                    "ExcludeCharacters": "",
+                    "ExcludeNumbers": False,
+                    "ExcludePunctuation": True,
+                    "ExcludeUppercase": True,
+                    "ExcludeLowercase": True,
+                    "IncludeSpace": False,
+                    "password": creator.password,
+                }
+            ),
+            headers=self.HEADERS,
+        )
+        secret_id = gen_secret_resp.json["message"].split("/")[-1]
+        resp = self.client.get(
+            f"/getSecret/{secret_id}",
+            headers=self.HEADERS,
+            data=json.dumps({"password": creator.password}),
+        )
+        print(resp)
+        self.assertEqual(200, resp.status_code)
+        another_resp = self.client.get(
+            f"/getSecret/{secret_id}",
+            headers=self.HEADERS,
+            data=json.dumps({"password": creator.password}),
+        )
+        print(another_resp)
+        self.assertEqual(404, another_resp.status_code)
+
+    def test_update_secret_with_predefined_secret(self):
+        """
+        Test the update secret route
+        """
+        creator = CreatorFactory()
+
+        reg_resp = self.client.post(
+            self.REGISTER_PATH,
+            data=json.dumps({"email": creator.email, "password": creator.password}),
+            headers=self.HEADERS,
+        )
+        self.HEADERS["Authorization"] = f"Bearer {reg_resp.json['token']}"
+        gen_secret_resp = self.client.post(
+            self.GENERATE_SECRET_PATH,
+            data=json.dumps(
+                {
+                    "PasswordLength": 255,
+                    "ExcludeCharacters": "",
+                    "ExcludeNumbers": False,
+                    "ExcludePunctuation": True,
+                    "ExcludeUppercase": True,
+                    "ExcludeLowercase": True,
+                    "IncludeSpace": False,
+                    "password": creator.password,
+                }
+            ),
+            headers=self.HEADERS,
+        )
+        secret_id = gen_secret_resp.json["message"].split("/")[-1]
+        resp = self.client.put(
+            f"/getSecret/{secret_id}",
+            headers=self.HEADERS,
+            data=json.dumps({"password": creator.password, "secret": "new secret"}),
+        )
+        self.assertEqual(200, resp.status_code)
+
+    def test_delete_secret(self):
+        """
+        Test the delete secret route
+        """
+        creator = CreatorFactory()
+
+        reg_resp = self.client.post(
+            self.REGISTER_PATH,
+            data=json.dumps({"email": creator.email, "password": creator.password}),
+            headers=self.HEADERS,
+        )
+        self.HEADERS["Authorization"] = f"Bearer {reg_resp.json['token']}"
+        gen_secret_resp = self.client.post(
+            self.GENERATE_SECRET_PATH,
+            data=json.dumps(
+                {
+                    "PasswordLength": 255,
+                    "ExcludeCharacters": "",
+                    "ExcludeNumbers": False,
+                    "ExcludePunctuation": True,
+                    "ExcludeUppercase": True,
+                    "ExcludeLowercase": True,
+                    "IncludeSpace": False,
+                    "password": creator.password,
+                }
+            ),
+            headers=self.HEADERS,
+        )
+        secret_id = gen_secret_resp.json["message"].split("/")[-1]
+        resp = self.client.delete(f"/deleteSecret/{secret_id}", headers=self.HEADERS,)
+        self.assertEqual(204, resp.status_code)
+
+    def test_s3_upload_object_without_file(self):
+        """
+        Test upload of object to S3
+        """
+        creator = CreatorFactory()
+
+        reg_resp = self.client.post(
+            self.REGISTER_PATH,
+            data=json.dumps({"email": creator.email, "password": creator.password}),
+            headers=self.HEADERS,
+        )
+        self.HEADERS["Authorization"] = f"Bearer {reg_resp.json['token']}"
+        resp = self.client.post(f"/upload", headers=self.HEADERS)
+        self.assertEqual(400, resp.status_code)
+
+    def test_s3_upload_object_with_file(self):
+        """
+        Test upload of object to S3
+        """
+        creator = CreatorFactory()
+        reg_resp = self.client.post(
+            self.REGISTER_PATH,
+            data=json.dumps({"email": creator.email, "password": creator.password}),
+            headers=self.HEADERS,
+        )
+
+        self.HEADERS["Authorization"] = f"Bearer {reg_resp.json['token']}"
+        data = {"test_file": "this is a test file"}
+        data = {key: str(value) for key, value in data.items()}
+        data["file"] = (BytesIO(b"abide"), "test.jpg")
+        resp = self.client.post(
+            f"/upload",
+            headers=self.HEADERS,
+            content_type="multipart/form-data",
+            data=data,
+        )
+        self.assertEqual(201, resp.status_code)
+
+    def test_s3_generate_url_for_sharing_without_object_name(self):
+        """
+        Test generate of shared url for file upload from third parties
+        """
+        creator = CreatorFactory()
+        reg_resp = self.client.post(
+            self.REGISTER_PATH,
+            data=json.dumps({"email": creator.email, "password": creator.password}),
+            headers=self.HEADERS,
+        )
+
+        self.HEADERS["Authorization"] = f"Bearer {reg_resp.json['token']}"
+        resp = self.client.get(f"/upload", headers=self.HEADERS, data=json.dumps({}),)
+        print(resp.json)
+        self.assertEqual(400, resp.status_code)
+
+    def test_s3_generate_url_for_sharing_with_object_name(self):
+        """
+        Test generate of shared url for file upload from third parties
+        """
+        creator = CreatorFactory()
+        reg_resp = self.client.post(
+            self.REGISTER_PATH,
+            data=json.dumps({"email": creator.email, "password": creator.password}),
+            headers=self.HEADERS,
+        )
+
+        self.HEADERS["Authorization"] = f"Bearer {reg_resp.json['token']}"
+        resp = self.client.get(
+            f"/upload",
+            headers=self.HEADERS,
+            data=json.dumps({"object_name": "test.jpg"}),
+        )
+        self.assertEqual(200, resp.status_code)
